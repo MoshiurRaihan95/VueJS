dc94f4c0073c65429e32e6b6d03810cde0c8179f
MoshiurRaihan95 <moshiurraihan95@gmail.com>
2023-07-10 16:16:15 +0600

    Preview date form var, array,Object

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,30 +1,28 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  const message = 'Welcome. Get ready to master Vue.js 3!'
+  const date = '30th june, 2023'
+  // Display current date with function
+  function getDate(){
+    const date = new Date();
+    return date.toDateString()
+  }
+
+  // Display current date with object
+  const data = {
+    message: 'Welcome. Get ready to master Vue.js 3!',
+    date: '30th june, 2023',
+    task: [1,2,3,4,5,6,'No']
+    }
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container mx-auto flex items-center justify-center min-h-screen flex-col">
+    <h1 class="text-4xl text-black">{{ data.message }}</h1>
+    <h1 class="mt-10 text-xl text-gray-700">Today is <span>{{ data.date }}</span></h1>
+    <h1 class="mt-10 text-2xl text-black-700">You have <span class="bg-slate-400 text-orange-600 p-1 rounded-lg">{{ data.task[6]}}</span> task for today.</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
